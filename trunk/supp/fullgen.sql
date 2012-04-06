8916072d631aeb4703e6989cd9b710af96f4ad71
Duane.Dieterich@gmail.com <Duane.Dieterich@gmail.com@ea9f4b38-b855-7c25-3528-d1489773a8d0>
2012-04-06 19:30:00 +0000

    Related Updates

--- trunk/supp/fullgen.sql
+++ trunk/supp/fullgen.sql
@@ -13,10 +13,29 @@ define APP_ID = DTGEN   -- APPLICATIONS.ABBR for the Application
 spool fullgen
 BEGIN
    util.set_usr('Initial Load');  -- Any string will work for this parameter
-   -- The following calls are equivalent to "generate.run('&APP_ID.');"
-   generate.cr('&APP_ID.');
-   generate.dr('&APP_ID.');
-   generate.gui('&APP_ID.');
+   generate.init('&APP_ID.');
+   -- Drop/Delete Scripts
+   generate.drop_usyn;
+   generate.drop_mods;
+   generate.drop_oltp;
+   generate.drop_dist;
+   generate.drop_integ;
+   generate.delete_ods;
+   generate.drop_ods;
+   generate.drop_gdst;
+   generate.drop_glob;
+   -- Create Scripts
+   generate.create_glob;
+   generate.create_gdst;
+   generate.create_ods;
+   generate.create_integ;
+   generate.create_dist;
+   generate.create_oltp;
+   generate.create_mods;
+   generate.create_usyn;
+   -- Create GUI Script
+   generate.create_flow;
+   commit;
 END;
 /
 spool off
@@ -53,3 +72,45 @@ set termout on
 set feedback 6
 set pagesize 20
 set linesize 80
+
+REM
+REM  ============================================================
+REM
+REM  The folowing creates individual files for each script
+REM
+REM  NOTE: This approach is an advanced option and requires
+REM     proper assembly of the install and uninstall files
+REM     that are created.  Use of the ASSEMBLE package above
+REM     is preferred.
+REM
+REM 
+REM @select_file &APP_ID. drop_usyn
+REM @select_file &APP_ID. drop_mods
+REM @select_file &APP_ID. drop_oltp
+REM @select_file &APP_ID. drop_dist
+REM @select_file &APP_ID. drop_integ
+REM @select_file &APP_ID. delete_ods
+REM @select_file &APP_ID. drop_ods
+REM @select_file &APP_ID. drop_gdst
+REM @select_file &APP_ID. drop_glob
+REM 
+REM @select_file &APP_ID. create_glob
+REM @select_file &APP_ID. create_glob_sec
+REM @select_file &APP_ID. create_gdst
+REM @select_file &APP_ID. create_gdst_sec
+REM @select_file &APP_ID. create_ods
+REM @select_file &APP_ID. create_ods_sec
+REM @select_file &APP_ID. create_integ
+REM @select_file &APP_ID. create_integ_sec
+REM @select_file &APP_ID. create_dist
+REM @select_file &APP_ID. create_dist_sec
+REM @select_file &APP_ID. create_oltp
+REM @select_file &APP_ID. create_oltp_sec
+REM @select_file &APP_ID. create_mods
+REM @select_file &APP_ID. create_mods_sec
+REM @select_file &APP_ID. create_usyn
+REM 
+REM @select_file &APP_ID. create_flow
+REM 
+REM @comp_file &APP_ID.
+REM 
