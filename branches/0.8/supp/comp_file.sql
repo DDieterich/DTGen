7f4e40145787adac6dbb8fe192fafded6c1aa365
Duane.Dieterich@gmail.com <Duane.Dieterich@gmail.com@ea9f4b38-b855-7c25-3528-d1489773a8d0>
2012-04-10 21:27:56 +0000

    DTGen Release 0.8

---
+++ branches/0.8/supp/comp_file.sql
@@ -0,0 +1,72 @@
+
+set linesize 4000
+set trimspool on
+set pagesize 0
+set feedback off
+set termout off
+set verify off
+set serveroutput on
+set define on
+
+spool comp.sql
+
+prompt
+prompt prompt
+prompt prompt === Compile Stored Program Units ===
+prompt
+prompt set define off
+
+begin
+   dbms_output.put_line('-- Package Specs');
+   for buff in (
+      select * from programs_act
+       where APPLICATIONS_NK1 = '&1.'
+        and  type = 'PACKAGE'
+	    order by name )
+   loop
+      dbms_output.put_line('@'||buff.name||'.pks');
+      dbms_output.put_line('show errors '||buff.type||' '||buff.name);
+   end loop;
+   dbms_output.put_line('-- Functions');
+   for buff in (
+      select * from programs_act
+       where APPLICATIONS_NK1 = '&1.'
+        and  type = 'FUNCTION'
+	    order by name )
+   loop
+      dbms_output.put_line('@'||buff.name||'.fnc');
+      dbms_output.put_line('show errors '||buff.type||' '||buff.name);
+   end loop;
+   dbms_output.put_line('-- Procedures');
+   for buff in (
+      select * from programs_act
+       where APPLICATIONS_NK1 = '&1.'
+        and  type = 'PROCEDURE'
+	    order by name )
+   loop
+      dbms_output.put_line('@'||buff.name||'.prc');
+      dbms_output.put_line('show errors '||buff.type||' '||buff.name);
+   end loop;
+   dbms_output.put_line('-- Package Bodies');
+   for buff in (
+      select * from programs_act
+       where APPLICATIONS_NK1 = '&1.'
+        and  type = 'PACKAGE'
+	    order by name )
+   loop
+      dbms_output.put_line('@'||buff.name||'.pkb');
+      dbms_output.put_line('show errors PACKAGE BODY '||buff.name);
+   end loop;
+end;
+/
+
+prompt set define on
+prompt
+
+spool off
+
+set verify on
+set termout on
+set feedback 6
+set pagesize 20
+set linesize 80
