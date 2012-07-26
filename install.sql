1def9e6fe22175d377aaff420e281c7dfef6b8a7
Duane.Dieterich@gmail.com <Duane.Dieterich@gmail.com@ea9f4b38-b855-7c25-3528-d1489773a8d0>
2012-07-26 22:31:30 +0000

    Re-arranged development and installation scripts

---
+++ install.sql
@@ -0,0 +1,39 @@
+
+REM
+REM DTGen Database Installation Script
+REM (Must be run as the "sys as sysdba" user)
+REM
+
+spool install
+
+REM Configure SQL*Plus
+REM
+WHENEVER SQLERROR EXIT SQL.SQLCODE
+WHENEVER OSERROR EXIT
+set define '&'
+set trimspool on
+set serveroutput on
+set feedback off
+set verify off
+
+REM Initialize Variables
+REM
+define NAME = dtgen   -- New Schema Owner Name
+define PASS = dtgen   -- New Schema Owner Password
+
+REM Create New Schema Owner
+REM
+-- New Schema Owner Default Tablespace:   users
+-- New Schema Owner Temporary Tablespace: temp
+@src/create_owner &NAME. &PASS. users temp
+
+REM Create DTGen Schema Objects
+REM
+connect &NAME./&PASS.
+@src/dtgen/install_db
+@src/dtgen/comp
+
+set feedback on
+set verify on
+
+spool off
---
+++ src/dtgen/uninstall.sql
@@ -0,0 +1,37 @@
+
+REM
+REM DTGEN Un-Installation Script
+REM (Must be run as the "system" or "sys as sysdba" user)
+REM
+
+spool uninstall
+
+REM Initialize Variables
+REM
+define OWNERNAME = dtgen     -- Schema Owner to be removed
+
+REM Configure SQL*Plus
+REM
+WHENEVER SQLERROR CONTINUE
+WHENEVER OSERROR CONTINUE
+set trimspool on
+set serveroutput on
+set feedback off
+set define on
+
+prompt
+prompt This will remove the following user from the database:
+prompt
+prompt   -) &OWNERNAME.
+prompt
+prompt Note: APEX Applications must be dropped manually
+prompt Note: DTGen users must be dropped manually
+prompt
+prompt Press ENTER to continue
+accept junk
+
+drop role &OWNERNAME._app;
+drop role &OWNERNAME._dml;
+drop user &OWNERNAME. cascade;
+
+spool off
