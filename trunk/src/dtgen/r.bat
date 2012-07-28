1218bf7c3032c227d597399650a008ead328d2ee
Duane.Dieterich@gmail.com <Duane.Dieterich@gmail.com@ea9f4b38-b855-7c25-3528-d1489773a8d0>
2012-07-28 02:45:42 +0000

    Re-arranged development and installation scripts

--- trunk/src/dtgen/r.bat
+++ trunk/src/dtgen/r.bat
@@ -1,7 +1,8 @@
+@ECHO OFF
 
 sqlplus dtgen/dtgen@XE2 @rebuild
 
 REM sqlldr dtgen/dtgen@XE2 CONTROL=dtgen_dataload.ctl
 fgrep -e " Rows not loaded due to data errors." -e " Rows not loaded because all fields were null." -e "Total logical records skipped: " -e "Total logical records rejected: " -e "Total logical records discarded: " dtgen_dataload.log
 
-fgrep -i -e err -e fail -e warn -e ora- -e sp2- -e pls- fullgen_dtgen.LST rebuild.LST
+fgrep -i -e fail -e warn -e ora- -e sp2- -e pls- fullgen_dtgen.LST rebuild.LST
