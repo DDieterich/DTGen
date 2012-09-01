0f909ea9c28261d4405f9679f581c8f2592e9047
Duane.Dieterich@gmail.com <Duane.Dieterich@gmail.com@ea9f4b38-b855-7c25-3528-d1489773a8d0>
2012-09-01 19:36:50 +0000

    Release 0.12 testing updates

--- test/create_ut_objs.sql
+++ test/create_ut_objs.sql
@@ -8,6 +8,7 @@
 -- Create Synonyms back to the Generator Schema Object Owner
 create synonym applications_act for &1..applications_act;
 create synonym file_lines_asof  for &1..file_lines_asof;
+create synonym util             for &1..util;
 create synonym glob             for &1..glob;
 create synonym generate         for &1..generate;
 
@@ -18,38 +19,42 @@ grant execute on test_rig to dtgen_ut_test;
 
 create table test_run as
    select abbr          app_abbr
-         ,db_schema     schema_name
+         ,db_schema     db_schema
          ,systimestamp  gen_tstamp
    from applications_act where 0 = 1;
 alter table test_run
    add (constraint test_run_pk
-   primary key (app_abbr, schema_name));
+   primary key (app_abbr, db_schema));
 grant select, insert, update, delete
    on test_run to dtgen_ut_test;
 
-create sequence test_parms_seq;
-create table test_parms
-   (schema_name  applications.db_schema%TYPE -- Primary Key
-   ,seq          number       -- Primary Key
-   ,test_name    varchar2(30)   not null
-   ,success      varchar2(4000) not null
-   ,val1         varchar2(4000)
-   ,val2         varchar2(4000)
-   ,constraint test_parms_pk primary key (schema_name, seq));
+drop table test_parms;
+create table test_parms as
+   select db_schema from applications_act
+    where 0 = 1;
+alter table test_parms
+  add (seq          number
+      ,test_name    varchar2(30)   not null
+      ,success      varchar2(4000) not null
+      ,val0         varchar2(4000)
+      ,val1         varchar2(4000)
+      ,val2         varchar2(4000)
+      ,val3         varchar2(4000)
+      ,val4         varchar2(4000)
+      ,val5         varchar2(4000)
+      ,val6         varchar2(4000)
+      ,val7         varchar2(4000)
+      ,val8         varchar2(4000)
+      ,val9         varchar2(4000)
+      ,constraint test_parms_pk primary key (db_schema, seq));
 grant select
    on test_parms to dtgen_ut_test;
-create trigger test_parms_bi
-  before insert on test_parms
-  for each row
-begin
-   select test_parms_seq.nextval into :new.seq from dual;
-end test_parms_bi;
 /
 
 @test_gen.pkb
 @test_rig.pkb
 
-insert into test_parms (schema_name, test_name, success, val1, val2, val3) values ('TDBST', 'DTC_INSERT', 'SUCCESS', 'SUCCESS', 'SUCCESS', 'NUM_PLAIN_PLAIN', 1, null);
-insert into test_parms (schema_name, test_name, success, val1, val2, val3) values ('TDBST', 'DTC_INSERT', 'SUCCESS', 'SUCCESS', 'SUCCESS', 'NUM_PLAIN_PLAIN', 2, '123.89');
-insert into test_parms (schema_name, test_name, success, val1, val2, val3) values ('TDBST', 'DTC_INSERT', 'SUCCESS', 'SUCCESS', 'SUCCESS', 'NUM_PLAIN_PLAIN', 3, '-1.2e-4');
-insert into test_parms (schema_name, test_name, success, val1, val2, val3) values ('TDBST', 'DTC_INSERT', 'SUCCESS', 'SUCCESS', 'SUCCESS', 'NUM_PLAIN_PLAIN', 4, '1.2e5');
+insert into test_parms (db_schema, seq, test_name, success, val0, val1, val2) values ('TDBST', 1, 'DTC_INSERT', 'SUCCESS', 'NUM_PLAIN', 1, null);
+insert into test_parms (db_schema, seq, test_name, success, val0, val1, val2) values ('TDBST', 2, 'DTC_INSERT', 'SUCCESS', 'NUM_PLAIN', 2, '123.89');
+insert into test_parms (db_schema, seq, test_name, success, val0, val1, val2) values ('TDBST', 3, 'DTC_INSERT', 'SUCCESS', 'NUM_PLAIN', 3, '-1.2e-4');
+insert into test_parms (db_schema, seq, test_name, success, val0, val1, val2) values ('TDBST', 4, 'DTC_INSERT', 'SUCCESS', 'NUM_PLAIN', 4, '1.2e5');
