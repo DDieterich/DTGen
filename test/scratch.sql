26064dc3f998e1116f72dbd03f852d80a9d62928
Duane.Dieterich@gmail.com <Duane.Dieterich@gmail.com@ea9f4b38-b855-7c25-3528-d1489773a8d0>
2012-09-30 03:48:07 +0000

    Release 0.12 testing updates, Date/Time Basic Table Test Implemented

--- test/scratch.sql
+++ test/scratch.sql
@@ -1,6 +1,16 @@
 ------------------------------------------------------------
 REM Test User
 
+execute glob.set_usr('Duane');
+
+insert into t1a_non(id, key, seq, tstamp_tz_plain, tstamp_ltz_plain) values(1000, 'Duane', 1,
+   to_timestamp_tz('31-DEC-9999 AD 23:59:59.999 UTC','DD-MON-YYYY AD HH24:MI:SS.FF3 TZR'),
+   to_timestamp('31-DEC-9999 AD 23:59:59.999','DD-MON-YYYY AD HH24:MI:SS.FF3'));
+
+select to_char(tstamp_tz_plain, 'DD-MON-YYYY AD HH24:MI:SS.FF3 TZD'),
+       to_char(tstamp_ltz_plain, 'DD-MON-YYYY AD HH24:MI:SS.FF3 TZD')
+ from  t1a_non where id = 1000;
+
 execute test_rig.run_all;
 execute test_rig.run_test('A','BTT_SQLTAB_NON_NUM_PLAIN');
 execute dbms_output.put_line(test_rig.BTT_SQLTAB_NON_NUM_PLAIN('BTT_NUM_PASS',2));
@@ -20,3 +30,13 @@ REM dtgen_dev:
 
 execute glob.set_usr('Test');
 execute gui_util.gen_all('TST1');
+
+-- copy columns
+insert into tab_cols_act (tables_nk1, tables_nk2, name, seq, nk, type, len, description)
+   select tables_nk1, 'T1AL', name, seq, nk, type, len, description
+    from  tab_cols_act where tables_nk1 = 'TST1' and tables_nk2 = 'T1AN'
+    and   seq > 20;
+insert into tab_cols_act (tables_nk1, tables_nk2, name, seq, nk, type, len, description)
+   select tables_nk1, 'T1AE', name, seq, nk, type, len, description
+    from  tab_cols_act where tables_nk1 = 'TST1' and tables_nk2 = 'T1AN'
+    and   seq > 20;
